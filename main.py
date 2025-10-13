1872e9d7e8c6c7c2cf84763fe4be9a9bab73efd4
Redefined <freakgaming2009@gmail.com>
2025-10-13 16:02:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1070,7 +1070,7 @@ def title():
     stdout.write(
         "             " + Fore.LIGHTCYAN_EX + "║ " + Fore.LIGHTWHITE_EX + "          Type [help] to see the Commands    " + Fore.LIGHTCYAN_EX + "       ║\n")
     stdout.write(
-        "             " + Fore.LIGHTCYAN_EX + "║ " + Fore.LIGHTWHITE_EX + "         Contact Dev - Telegram @AuraNetz   " + Fore.LIGHTCYAN_EX + "        ║\n")
+        "             " + Fore.LIGHTCYAN_EX + "║ " + Fore.LIGHTWHITE_EX + "         Contact Dev - Discord - nightt.js   " + Fore.LIGHTCYAN_EX + "        ║\n")
     stdout.write("             " + Fore.LIGHTCYAN_EX + "╚═════════════════════════════════════════════════════╝\n")
     stdout.write("\n")
 
